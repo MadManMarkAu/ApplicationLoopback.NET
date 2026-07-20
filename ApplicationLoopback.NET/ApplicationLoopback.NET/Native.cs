b4e075299875b9df78d48d5a8cc50b22aa61a492
Frooxius <tomas.mariancik@gmail.com>
2026-07-20 22:03:02 +0200

    feat: Add initial C# part with a test

---
+++ ApplicationLoopback.NET/ApplicationLoopback.NET/Native.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ApplicationLoopback.NET
+{
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    public delegate void AudioCallback(IntPtr instance, IntPtr data, uint length);
+
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    public delegate void AudioEvent(IntPtr instance);
+
+    public static class Native
+    {
+        [DllImport("ApplicationLoopback.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr InitializeCapture(ushort channels, uint sampleRate, ushort bitsPerSample, 
+            AudioCallback callback, AudioEvent audioCaptureStopped);
+
+        [DllImport("ApplicationLoopback.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr GetCallback(IntPtr capture);
+
+        [DllImport("ApplicationLoopback.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int StartCaptureAsync(IntPtr capture, uint processId, bool includeProcessTree);
+
+        [DllImport("ApplicationLoopback.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int StopCaptureAsync(IntPtr capture);
+
+        [DllImport("ApplicationLoopback.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern void FreeCapture(IntPtr capture);
+    }
+}
---
+++ ApplicationLoopback.NET/Sample/Program.cs
@@ -0,0 +1,30 @@
+﻿using ApplicationLoopback.NET;
+using System.Runtime.InteropServices;
+
+var callback = (AudioCallback)OnData;
+var stopped = (AudioEvent)OnStopped;
+
+var capture = Native.InitializeCapture(2, 48000, 16, callback, stopped);
+Console.WriteLine($"Created capture: {capture}");
+
+var startResult = Native.StartCaptureAsync(capture, 41912, true);
+
+Console.WriteLine(startResult);
+Console.ReadLine();
+Console.WriteLine("Stopping");
+
+Native.StopCaptureAsync(capture);
+
+Console.ReadLine();
+
+Native.FreeCapture(capture);
+
+static void OnData(IntPtr instance, IntPtr data, uint length)
+{
+    Console.WriteLine($"Got data: {length}");
+}
+
+static void OnStopped(IntPtr instance)
+{
+    Console.WriteLine("Audio capture stopped");
+}
